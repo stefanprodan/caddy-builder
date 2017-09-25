35e9bb4b5fac35ea5efea76408571ea8f35099be
Stefan Prodan <stefan.prodan@gmail.com>
2017-09-26 00:31:18 +0300

    install ipfilter

--- plugins.go
+++ plugins.go
@@ -3,6 +3,8 @@ package caddyhttp
 import (
 	// http.prometheus
 	_ "github.com/miekg/caddy-prometheus"
+	// http.ipfilter
+	_ "github.com/pyed/ipfilter"
 	// http.filemanager
 	_ "github.com/hacdias/filemanager/caddy/filemanager"
 	// http.hugo
